ace1320e849c4ffe447e047bf51a3841e0ae946f
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-11-26 11:35:59 +0100

    ADD struct

---
+++ L07/E01/E01.c
@@ -0,0 +1,14 @@
+#include <inttypes.h>
+#include <stdio.h>
+
+typedef struct Collana { // Struttura rappresentante una collana
+    unsigned int Zaffiri;
+    unsigned int Smeraldi;
+    unsigned int Topazi;
+    uint8_t *Array;
+} collana;
+
+int main() {
+    /* code */
+    return 0;
+}
