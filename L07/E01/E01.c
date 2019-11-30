61e9da0f9c59fd10538c9748a3bb8e9054583b23
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-11-30 17:35:06 +0100

    ADD disp_ripet

--- L07/E01/E01.c
+++ L07/E01/E01.c
@@ -101,16 +101,34 @@ bool apriFile(char *filename, char *modalità, FILE **stream) { // Apre un file 
     return true;
 }
 
-void generaCollane ()
+/*
+k = numero di ripetizioni massime per ogni singolo elemento
+count conta il numero di soluzioni
+*/
+unsigned int disp_ripet(unsigned int pos, unsigned int *val, unsigned int *sol, unsigned int n, unsigned int k) {
+    unsigned int count = 0;              // Contatore per il numero di soluzioni
+    if (pos >= k) {                      // Condizione di terminazione, ovvero quando la posizione raggiunge il numero massimo di ripetizioni
+        for (size_t i = 0; i < k; i++) { // Per il numero di ripetizioni massime
+            printf("%d", sol[i]);        // Stampo ogni elemento della soluzione
+        }
+        printf("\n");
+        return 1;
+    }
+    for (size_t i = 0; i < n; i++) { // Per ogni elemento dell'array dei valori
+        sol[pos] = val[i];
+        count += disp_ripet(pos + 1, val, sol, n, k); // Ricorsione nella posizione successiva
+    }
+    return count;
+}
 
 int main() {
     char filename[MAX_FILENAME];
     puts("Inserisci il numero di pietre secondo quest'ordine: z s r t");
     printf("==> ");
     fgets(filename, (MAX_FILENAME - 1), stdin);
-    collana c = parseCollana(filename);
+    collana c   = parseCollana(filename);
     collana sol = c;
-    sol.Array = (uint8_t *)calloc(sol.TotalePietre, sizeof(uint8_t));
+    sol.Array   = (uint8_t *)calloc(sol.TotalePietre, sizeof(uint8_t));
     disp_ripet(0, &c, &sol, 1);
     return 0;
 }
