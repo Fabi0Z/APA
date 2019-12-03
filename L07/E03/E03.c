8042b1813b467fb10381ccb014ca6933f6a80af4
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-03 22:55:24 +0100

    ADD personaggio link

--- L07/E03/E03.c
+++ L07/E03/E03.c
@@ -6,7 +6,6 @@
 
 const uint8_t MAX_STRING = 50;
 
-typedef struct Personaggio *personaggioLink;
 typedef struct Personaggio {
     uint16_t ID;
     char *Nome;
@@ -17,8 +16,13 @@ typedef struct Personaggio {
     uint16_t DEF;
     uint16_t MAG;
     uint16_t SPR;
-    personaggioLink Next;
 } personaggio;
+
+typedef struct PersonaggioLink *personaggioLink;
+typedef struct PersonaggioLink {
+    personaggio Personaggio;
+    personaggioLink Next;
+} personaggioLink;
 
 bool checkFilestream(FILE *stream) { // Controlla errori di apertura del file
     if (stream == NULL) {
@@ -27,22 +31,45 @@ bool checkFilestream(FILE *stream) { // Controlla errori di apertura del file
     return false;
 }
 
+personaggio creaPersonaggio(uint16_t ID, char *Nome, char *Classe, uint16_t HP, uint16_t MP,
+                            uint16_t ATK, uint16_t DEF, uint16_t MAG, uint16_t SPR) {
+    // Crea un personaggio a partire dai dati in ingresso
+
+    // Alloco i dati in memoria
+    personaggio p;
+    p.Nome   = (char *)calloc(strlen(Nome), sizeof(char));
+    p.Classe = (char *)calloc(strlen(Classe), sizeof(char));
+
+    // Copio il contenuto
+    p.ID  = ID;
+    p.HP  = HP;
+    p.MP  = MP;
+    p.ATK = ATK;
+    p.DEF = DEF;
+    p.MAG = MAG;
+    p.SPR = SPR;
+    strcpy(p.Nome, Nome);
+    strcpy(p.Classe, Classe);
+
+    return p;
+}
+
 personaggioLink creaPersonaggioLink(personaggio *p) { // Salva un personaggio in un elemento di tipo personaggioLink
     // Alloco la memoria
-    personaggioLink l = (personaggioLink)malloc(sizeof(personaggio));
-    l->Nome           = (char *)calloc(strlen(p->Nome), sizeof(char));
-    l->Classe         = (char *)calloc(strlen(p->Classe), sizeof(char));
+    personaggioLink l;
+    l.Personaggio.Nome   = (char *)calloc(strlen(p->Nome), sizeof(char));
+    l.Personaggio.Classe = (char *)calloc(strlen(p->Classe), sizeof(char));
 
     // Copio i dati
-    *l = *p;
-    strcpy(l->Nome, p->Nome);
-    strcpy(l->Classe, p->Classe);
+    l.Personaggio = *p;
+    strcpy(l.Personaggio.Nome, p->Nome);
+    strcpy(l.Personaggio.Classe, p->Classe);
 
     return l;
 }
 
 personaggioLink parsePersonaggi(FILE *stream) { // Legge i personaggi da file e li salva in una lista
-    personaggioLink head = (personaggioLink)malloc(sizeof(personaggio));
+    personaggioLink head = ;
 }
 
 int main() {
