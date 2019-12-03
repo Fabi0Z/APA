c8d3be5e72774f2f31bea2ab0e726cd73b7a2be3
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-03 22:19:15 +0100

    ADD filestream opening

--- L07/E03/E03.c
+++ L07/E03/E03.c
@@ -1,6 +1,25 @@
+#include <stdbool.h>
 #include <stdio.h>
 
+bool checkFilestream(FILE *stream) { // Controlla errori di apertura del file
+    if (stream == NULL) {
+        puts("Errore apertura file");
+    }
+    return false;
+}
+
 int main() {
-    /* code */
+    // Apro i filestream
+    FILE *pg         = fopen("pg.txt", "r");
+    FILE *inventario = fopen("inventario.txt", "R");
+
+    // Verifico i filestream
+    if (!checkFilestream(pg)) {
+        return 1;
+    }
+    if (!checkFilestream(inventario)) {
+        return 2;
+    }
+
     return 0;
 }
