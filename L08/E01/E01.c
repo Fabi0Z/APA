ca10d0aa76b2a8061ac81fbcae86c47da570ab03
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-05 23:14:04 +0100

    ADD attività sovvrapposte

--- L08/E01/E01.c
+++ L08/E01/E01.c
@@ -10,13 +10,16 @@ typedef struct Attivita {
 } attivita;
 // * -------------------------------------------------------------
 
-// * FUNZIONI GENERALI
+// * FUNZIONI DI CONTROLLO
 bool checkFilestream(FILE *stream) { // Controlla errori di apertura del file
     if (stream == NULL) {
         puts("Errore apertura file");
         exit(1);
     }
     return true;
+}
+bool attivitaSovvrapposta(attivita *a, attivita *b) { // Controlla se due attività sono sovvrapposte
+    return a->Inizio < b->Fine && b->Inizio < a->Fine;
 }
 // * -------------------------------------------------------------
 
@@ -45,7 +48,49 @@ void printArrayAttivita(attivita *a, uint8_t numeroAttivita) { // Stampa un arra
         printAttivita(&a[i]);
     }
     printf("\n");
-} /* code */
+}
+// * -------------------------------------------------------------
+
+// * MANIPOLAZIONE DATI
+// void merge(corsa *corse, corsa *corseAppoggio, int inzio, int centro, int fine, ordinamento ordine) { // Unisce i due sottovettori ordinandoli
+//     int i, j;
+//     i = inzio;      // Contatore per l'esporazione dell'array sinistro
+//     j = centro + 1; // Contatore per l'esporazione dell'array destro
+
+//     int k;
+//     for (k = inzio; i <= centro && j <= fine; k++) {      // Confronto gli array sinistro e destro
+//         if (confrontaCampi(&corse[j], &corse[i], ordine)) // Se l'elemento nel sinistro è minore o uguale (oppure quello destro maggiore o uguale) all'altro copio di conseguenza
+//             corseAppoggio[k] = corse[i++];
+//         else
+//             corseAppoggio[k] = corse[j++];
+//     }
+
+//     while (i <= centro)
+//         corseAppoggio[k++] = corse[i++]; // Copio il resto degli elementi a sinistra nell'array d'appoggio
+//     while (j <= fine)
+//         corseAppoggio[k++] = corse[j++]; // Copio il resto degli elementi a destra nell'array d'appoggio
+//     for (k = inzio; k <= fine; k++)
+//         corse[k] = corseAppoggio[k]; // Ricopio tutto nell'array originale
+// }
+
+// void ordinaRicorsivo(corsa *corse, corsa *corseAppoggio, int inizio, int fine, ordinamento ordine) { // Componente ricorsiva del Merge Sort
+//     int centro = (inizio + fine) / 2;                                                                // Calcolo l'indice di metà vettore
+
+//     if (inizio >= fine) { // Se ho un vettore unitario mi interrompo
+//         return;
+//     }
+
+//     ordinaRicorsivo(corse, corseAppoggio, inizio, centro, ordine);   // Ordina a sinistra
+//     ordinaRicorsivo(corse, corseAppoggio, centro + 1, fine, ordine); // Ordina a destra
+//     merge(corse, corseAppoggio, inizio, centro, fine, ordine);       // Unisco i due array
+// }
+
+// void ordina(int numeroCorse, corsa corse[numeroCorse], ordinamento ordine) { // Funzione che ordina il log in base a come richiesto (è un Merge Sort)
+//     corsa *corseAppoggio = (corsa *)malloc(numeroCorse * sizeof(corsa));     // Creo l'array d'appoggio
+//     int inizio = 0, fine = numeroCorse - 1;
+//     ordinaRicorsivo(corse, corseAppoggio, inizio, fine, ordine);
+//     ORDINE_ATTUALE = ordine;
+// }
 // * -------------------------------------------------------------
 
 int main() {
