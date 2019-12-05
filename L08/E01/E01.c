11fb024b1a34e84b330f8c1e906892ada9a4f328
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-05 23:24:35 +0100

    ADD ordinamento

--- L08/E01/E01.c
+++ L08/E01/E01.c
@@ -52,45 +52,41 @@ void printArrayAttivita(attivita *a, uint8_t numeroAttivita) { // Stampa un arra
 // * -------------------------------------------------------------
 
 // * MANIPOLAZIONE DATI
-// void merge(corsa *corse, corsa *corseAppoggio, int inzio, int centro, int fine, ordinamento ordine) { // Unisce i due sottovettori ordinandoli
-//     int i, j;
-//     i = inzio;      // Contatore per l'esporazione dell'array sinistro
-//     j = centro + 1; // Contatore per l'esporazione dell'array destro
+void merge(attivita *a, attivita *b, int inzio, int centro, int fine) { // Unisce i due sottovettori ordinandoli
+    uint8_t i, j;
+    i = inzio;      // Contatore per l'esporazione dell'array sinistro
+    j = centro + 1; // Contatore per l'esporazione dell'array destro
 
-//     int k;
-//     for (k = inzio; i <= centro && j <= fine; k++) {      // Confronto gli array sinistro e destro
-//         if (confrontaCampi(&corse[j], &corse[i], ordine)) // Se l'elemento nel sinistro è minore o uguale (oppure quello destro maggiore o uguale) all'altro copio di conseguenza
-//             corseAppoggio[k] = corse[i++];
-//         else
-//             corseAppoggio[k] = corse[j++];
-//     }
+    int k;
+    for (k = inzio; i <= centro && j <= fine; k++) { // Confronto gli array sinistro e destro
+        if (a[i].Inizio <= a[j].Inizio)              // Se l'elemento nel sinistro è minore o uguale (oppure quello destro maggiore o uguale) all'altro copio di conseguenza
+            b[k] = a[i++];
+        else
+            b[k] = a[j++];
+    }
 
-//     while (i <= centro)
-//         corseAppoggio[k++] = corse[i++]; // Copio il resto degli elementi a sinistra nell'array d'appoggio
-//     while (j <= fine)
-//         corseAppoggio[k++] = corse[j++]; // Copio il resto degli elementi a destra nell'array d'appoggio
-//     for (k = inzio; k <= fine; k++)
-//         corse[k] = corseAppoggio[k]; // Ricopio tutto nell'array originale
-// }
+    while (i <= centro)
+        b[k++] = a[i++]; // Copio il resto degli elementi a sinistra nell'array d'appoggio
+    while (j <= fine)
+        b[k++] = a[j++]; // Copio il resto degli elementi a destra nell'array d'appoggio
+    for (k = inzio; k <= fine; k++)
+        a[k] = b[k]; // Ricopio tutto nell'array originale
+}
+void ordinaRicorsivo(attivita *a, attivita *b, uint8_t inizio, uint8_t fine) { // Componente ricorsiva del Merge Sort
+    int centro = (inizio + fine) / 2;                                          // Calcolo l'indice di metà vettore
+    if (inizio >= fine) {                                                      // Se ho un vettore unitario mi interrompo
+        return;
+    }
 
-// void ordinaRicorsivo(corsa *corse, corsa *corseAppoggio, int inizio, int fine, ordinamento ordine) { // Componente ricorsiva del Merge Sort
-//     int centro = (inizio + fine) / 2;                                                                // Calcolo l'indice di metà vettore
-
-//     if (inizio >= fine) { // Se ho un vettore unitario mi interrompo
-//         return;
-//     }
-
-//     ordinaRicorsivo(corse, corseAppoggio, inizio, centro, ordine);   // Ordina a sinistra
-//     ordinaRicorsivo(corse, corseAppoggio, centro + 1, fine, ordine); // Ordina a destra
-//     merge(corse, corseAppoggio, inizio, centro, fine, ordine);       // Unisco i due array
-// }
-
-// void ordina(int numeroCorse, corsa corse[numeroCorse], ordinamento ordine) { // Funzione che ordina il log in base a come richiesto (è un Merge Sort)
-//     corsa *corseAppoggio = (corsa *)malloc(numeroCorse * sizeof(corsa));     // Creo l'array d'appoggio
-//     int inizio = 0, fine = numeroCorse - 1;
-//     ordinaRicorsivo(corse, corseAppoggio, inizio, fine, ordine);
-//     ORDINE_ATTUALE = ordine;
-// }
+    ordinaRicorsivo(a, b, inizio, centro);   // Ordina a sinistra
+    ordinaRicorsivo(a, b, centro + 1, fine); // Ordina a destra
+    merge(a, b, inizio, centro, fine);       // Unisco i due array
+}
+void ordina(attivita *a, uint8_t numeroAttivita) {                          // Merge Sort
+    attivita *b    = (attivita *)malloc(numeroAttivita * sizeof(attivita)); // Creo l'array d'appoggio
+    uint8_t inizio = 0, fine = numeroAttivita - 1;
+    ordinaRicorsivo(a, b, inizio, fine);
+}
 // * -------------------------------------------------------------
 
 int main() {
@@ -101,6 +97,7 @@ int main() {
     fscanf(stream, "%" SCNd8 "\n", &numeroAttivita);
     attivita a[numeroAttivita];
     leggiAttivita(stream, a, numeroAttivita);
+    ordina(a, numeroAttivita);
     printArrayAttivita(a, numeroAttivita);
     return 0;
 }
