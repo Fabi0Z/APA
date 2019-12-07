6363c89f5ff1ebeadde004cdbff63376b6383144
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-07 16:09:12 +0100

    ADD powerset

--- L08/E01/E01.c
+++ L08/E01/E01.c
@@ -1,11 +1,28 @@
+#include "attivita.h"
+#include "smartfunctions.h"
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "attivita.h"
 
 // * CALCOLO COMBINATORIO
-// TODO 
+
+// Powerset
+void powerset(uint8_t posizione, attivita *a, attivita *soluzione,
+              uint8_t maxLunghezza, uint8_t start) {
+
+    if (start >= maxLunghezza) {                 // Se ho raggiunto la lunghezza massima
+        printArrayAttivita(soluzione, posizione); // Stampo soluzione
+        return;
+    }
+
+    for (uint8_t i = start; i < maxLunghezza; i++) { // Per ogni posizione rimanente
+        soluzione[posizione] = a[i];
+        powerset(posizione + 1, a, soluzione, maxLunghezza, i + 1);
+    }
+
+    powerset(posizione, a, soluzione, maxLunghezza, maxLunghezza);
+}
 // * -------------------------------------------------------------
 
 int main() {
@@ -18,5 +35,8 @@ int main() {
     leggiAttivita(stream, a, numeroAttivita);
     ordina(a, numeroAttivita);
     printArrayAttivita(a, numeroAttivita);
+
+    attivita b[numeroAttivita];
+    powerset(0, a, b, numeroAttivita, 0);
     return 0;
 }
