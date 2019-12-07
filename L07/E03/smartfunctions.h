9d999ab94bca325a767d3e35eca4e1dc40c506df
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-07 15:31:08 +0100

    ADD smartfunctions

---
+++ L07/E03/smartfunctions.h
@@ -0,0 +1,11 @@
+#if !defined(SMARTFUNCTIONS_H)
+#define SMARTFUNCTIONS_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+bool checkFilestream(FILE *stream);
+void premiPerContinuare();
+
+#endif // SMARTFUNCTIONS_H
