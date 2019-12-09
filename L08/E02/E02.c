6836bb6aa70d9d2e76b7d790b470dc8fe6092b6f
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-09 22:40:53 +0100

    ADD tessereDisponibili

--- L08/E02/E02.c
+++ L08/E02/E02.c
@@ -1,6 +1,41 @@
 #include "scacchiera.h"
 #include "smartfunctions.h"
 
+// Trova tutte le tessere non ancora utilizzate e le inserisce in un array di puntatori a tessere
+arrayTessera tessereDisponibili(arrayTessera *a, scacchiera *s) {
+    tessera *inUso[a->NumeroElementi];
+
+    // Controllo le tessere in uso
+    unsigned int posInUso = 0;
+    for (unsigned int i = 0; i < s->Righe; i++) {             // Per ogni riga
+        for (unsigned int j = 0; j < s->Colonne; j++) {       // Per ogni colonna
+            if (s->Matrice[i][j].Tessera != NULL) {           // Se la cella non è vuota
+                inUso[posInUso++] = s->Matrice[i][j].Tessera; // La segno tra le tessere in uso
+            }
+        }
+    }
+
+    arrayTessera tessereDisponibili;
+    tessereDisponibili.NumeroElementi = a->NumeroElementi - posInUso;
+    tessereDisponibili.Array          = (tessera **)calloc(tessereDisponibili.NumeroElementi, sizeof(tessera *));
+
+    unsigned int posLibere = 0;
+    for (unsigned int i = 0; i < a->NumeroElementi; i++) { // Per ogni tessera
+        bool checkUso = false;
+        for (unsigned int j = 0; j < posInUso; j++) { // Per ogni tessera in uso
+            if (a->Array[i] == inUso[j]) {            // Se è in uso
+                checkUso = true;
+                break; // Mi interrompo
+            }
+        }
+        if (!checkUso) {
+            tessereDisponibili.Array[posLibere++] = a->Array[i]; // La segno come non in uso
+        }
+    }
+
+    return tessereDisponibili;
+}
+
 int main(int argc, char const *argv[]) {
     FILE *tilesStream = smartFopen("data/tiles.txt", "r");
     unsigned int numeroTessere;
@@ -14,6 +49,10 @@ int main(int argc, char const *argv[]) {
     FILE *boardStream = smartFopen("data/board.txt", "r");
     scacchiera s;
     parseScacchiera(&s, boardStream, &a);
-    printScacchiera(&s, &a, stdout);
+    printScacchiera(&s, stdout);
+
+    puts("\nLe tessere libere sono:");
+    arrayTessera disponibili = tessereDisponibili(&a, &s);
+    printArrayTessera(&disponibili, stdout);
     return 0;
 }
