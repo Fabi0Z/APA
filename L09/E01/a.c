a1f635107bc69969f08c6eff9a79efbedef9b5f7
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-14 09:03:02 +0100

    Improve for unsigned int

--- L09/E01/a.c
+++ L09/E01/a.c
@@ -4,34 +4,34 @@
 #include <stdlib.h>
 
 typedef struct Array {
-    int *array;
-    int elementi;
+    unsigned int *array;
+    unsigned int elementi;
 } array;
 
-void printLIS(array *valori, array *posizioni, int index) {
-    if (index == -1) {
+void printLIS(array *valori, array *posizioni, unsigned int index) {
+    if (index == 0) {
         printf("\n");
         return;
     }
 
-    printf("%d ", valori->array[index]);
-    index = posizioni->array[index];
+    printf("%d ", valori->array[index - 1]);
+    index = posizioni->array[index - 1];
     printLIS(valori, posizioni, index);
 }
 
-int LISS(array *valori, array *posizioni, array *LIS) {
-    int precedente      = INT_MIN;
-    int precedenteIndex = -1;
-    int LISAttuale      = 1;
-    for (int i = 0; i < valori->elementi; i++) { // Per ogni elemento
+unsigned int LISS(array *valori, array *posizioni, array *LIS) {
+    unsigned int precedente      = 0;
+    unsigned int precedenteIndex = 0;
+    unsigned int LISAttuale      = 1;
+    for (unsigned int i = 0; i < valori->elementi; i++) { // Per ogni elemento
         if (valori->array[i] <= precedente) {    // Se il valore interrompe la sequenza
             // Inserisco i valori di interruzione
-            posizioni->array[i] = -1;
+            posizioni->array[i] = 0;
             LIS->array[i]       = 1;
         } else {
             // Proseguo l'incremento della LIS
             posizioni->array[i] = precedenteIndex;
-            precedenteIndex     = i;
+            precedenteIndex     = i + 1;
             LIS->array[i]       = LISAttuale++;
         }
         precedente = valori->array[i];
@@ -40,19 +40,19 @@ int LISS(array *valori, array *posizioni, array *LIS) {
     return precedenteIndex; // Restituisco l'ultimo valore della LIS
 }
 
-int main() {
+unsigned int main() {
     array valori;
     valori.elementi = 7;
-    int temp[]      = {6, 3, 5, 2, 7, 8, 1};
+    unsigned int temp[]      = {6, 3, 5, 2, 7, 8, 1};
     valori.array    = temp;
 
     array LIS, posizioni;
     LIS.elementi       = valori.elementi;
     posizioni.elementi = valori.elementi;
-    LIS.array          = (int *)calloc(valori.elementi, sizeof(int));
-    posizioni.array    = (int *)calloc(valori.elementi, sizeof(int));
+    LIS.array          = (unsigned int *)calloc(valori.elementi, sizeof(unsigned int));
+    posizioni.array    = (unsigned int *)calloc(valori.elementi, sizeof(unsigned int));
 
-    int indice = LISS(&valori, &posizioni, &LIS);
+    unsigned int indice = LISS(&valori, &posizioni, &LIS);
     printLIS(&valori, &posizioni, indice);
     return 0;
 }
