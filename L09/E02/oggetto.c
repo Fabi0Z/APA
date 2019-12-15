767ddf59dc9c6e7bde7ab5177835919d857ba3c9
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-15 15:55:47 +0100

    ADD arrayOggetti

--- L09/E02/oggetto.c
+++ L09/E02/oggetto.c
@@ -6,6 +6,11 @@ struct Oggetto {
     stats Statistiche;
 };
 
+struct ArrayOggetti {
+    oggetto *Array;
+    unsigned int NumeroOggetti;
+};
+
 // Alloca memoria per un oggetto di tipo oggetto
 void allocaOggetto(oggetto o, unsigned int nomeSize, unsigned int tipoSize) {
     o->Nome        = (char *)calloc(nomeSize, sizeof(char)); // Alloco la memoria
@@ -19,6 +24,16 @@ void freeOggetto(oggetto o) {
     free(o->Tipo);
     free(o->Statistiche);
     free(o);
+}
+
+// Dealloca un elemento di tipo arrayOggetto, se il parametro elementi è true libera anche gli oggetti dentro l'array
+void freeArrayOggetti(arrayOggetti a, bool elementi) {
+    if (elementi) {
+        for (unsigned int i = 0; i < a->NumeroOggetti; i++) { // Per ogni oggetto
+            freeOggetto(a->Array[i]);
+        }
+    }
+    free(a);
 }
 
 // Stampa un oggetto su file
@@ -35,11 +50,27 @@ oggetto creaOggetto(unsigned int nomeSize, unsigned int tipoSize) {
     return temp;
 }
 
+// Crea e restituisce un array di oggetti non allocati
+arrayOggetti creaArrayOggetti(unsigned int NumeroOggetti) {
+    arrayOggetti a;
+    a->NumeroOggetti = NumeroOggetti;
+    a->Array         = (oggetto *)calloc(NumeroOggetti, sizeof(oggetto));
+    return a;
+}
+
 // Copia src in dest
 void copiaOggetto(oggetto dest, oggetto src) {
     memcpy(dest->Statistiche, src->Statistiche, sizeof(int16_t) * N_STATISTICHE);
     strcpy(dest->Nome, src->Nome);
     strcpy(dest->Tipo, src->Tipo);
+}
+
+// Copia gli oggetti di src in dest
+void copiaArrayOggetti(arrayOggetti dest, arrayOggetti src) {
+    unsigned int min = dest->NumeroOggetti < src->NumeroOggetti ? dest->NumeroOggetti : src->NumeroOggetti;
+    for (unsigned int i = 0; i < min; i++) { // Per il numero minimo di oggetti
+        copiaOggetto(dest->Array[i], src->Array[i]);
+    }
 }
 
 // Effettua il parse di un personaggio da stringa, restituisce se la lettura è andata a buon fine o meno
@@ -58,10 +89,38 @@ bool leggiOggetto(char *string, oggetto o) {
     return conteggio == 4;
 }
 
+// Aggiunge un oggetto ad un array di oggetti
+void aggiungiOggetto(arrayOggetti a, oggetto o) {
+    if (a->NumeroOggetti == 0) { // Se non ho oggetti
+        freeArrayOggetti(a, false);
+        a           = creaArrayOggetti(1);
+        a->Array[0] = o;
+        return;
+    }
+
+    // Creo nuovo array oggetti
+    arrayOggetti new = creaArrayOggetti(a->NumeroOggetti + 1);
+    memcpy(new->Array, a->Array, sizeof(oggetto *) * a->NumeroOggetti);
+
+    // Inserisco il nuovo oggetto
+    new->Array[a->NumeroOggetti] = o;
+
+    // Sostituisco l'array di oggetti
+    a->Array = new->Array;
+    freeArrayOggetti(new, false);
+}
+
+// Restituisce il nome di un oggetto
 char *getNomeOggetto(oggetto o) {
     return o->Nome;
 }
 
+// Restituisce il tipo di un oggetto
 char *getTipoOggetto(oggetto o) {
     return o->Tipo;
+}
+
+// Restituisce le statistiche di un oggetto
+stats getStatisticheOggetto(oggetto o) {
+    return o->Statistiche;
 }
--- L09/E02/oggetto.h
+++ L09/E02/oggetto.h
@@ -2,15 +2,20 @@
 #define OGGETTO_H
 
 #include "statistiche.h"
-
 typedef struct Oggetto *oggetto;
+typedef struct ArrayOggetti *arrayOggetti;
 
+stats getStatisticheOggetto(oggetto o);
 char *getTipoOggetto(oggetto o);
 char *getNomeOggetto(oggetto o);
+void aggiungiOggetto(arrayOggetti a, oggetto o);
 bool leggiOggetto(char *string, oggetto o);
+void copiaArrayOggetti(arrayOggetti dest, arrayOggetti src);
 void copiaOggetto(oggetto dest, oggetto src);
+arrayOggetti creaArrayOggetti(unsigned int NumeroOggetti);
 oggetto creaOggetto(unsigned int nomeSize, unsigned int tipoSize);
 void printOggetto(oggetto o, FILE *stream);
+void freeArrayOggetti(arrayOggetti a, bool elementi);
 void freeOggetto(oggetto o);
 void allocaOggetto(oggetto o, unsigned int nomeSize, unsigned int tipoSize);
 
