d5ebd88a68de6e329a8114563405b95b6a939e7c
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-16 12:44:16 +0100

    UPDATE personaggio

--- L09/E02/personaggio.h
+++ L09/E02/personaggio.h
@@ -12,39 +12,15 @@ typedef struct Personaggio {
     stats Statistiche;
 } personaggio;
 
-typedef struct PersonaggioLink personaggioLink;
-typedef struct PersonaggioLink {
-    personaggio *Personaggio;
-    personaggioLink *Next;
-} personaggioLink;
-
-typedef struct TabellaPersonaggio {
-    personaggioLink *HEAD;
-    personaggioLink *TAIL;
-    unsigned int NumeroPersonaggi;
-} tabellaPersonaggio;
-
-void eliminaAndPrint(personaggioLink *previous);
-personaggioLink *estraiNext(personaggioLink *previous);
-personaggioLink *ricercaID(personaggioLink *HEAD, uint16_t *ID);
-personaggioLink *ricercaIDprecedente(personaggioLink *HEAD, uint16_t *ID);
-void addNext(personaggioLink *l, personaggioLink *next);
-void parsePersonaggi(tabellaPersonaggio *TABLE, FILE *stream);
+bool rimuoviEquipaggiamento(personaggio *p, oggetto o);
+void printPersonaggio(personaggio *p, FILE *stream);
 bool leggiPersonaggio(char *string, personaggio *p);
 personaggio *getResizedPersonaggio(personaggio *temp);
-void copiaPersonaggio(personaggio *dest, personaggio *src);
-personaggioLink *creaPersonaggioLink(personaggio *p);
-personaggio *creaPersonaggio(unsigned int nomeSize, unsigned int classeSize);
-personaggioLink *creaLista(personaggio *p);
-personaggioLink *getNextItem(personaggioLink *item);
-void printPersonaggioLink(personaggioLink *l);
-void printPersonaggioLinkFile(personaggioLink *l, FILE *stream);
-void printPersonaggio(personaggio *p, FILE *stream);
-void rimuoviEquipaggiamento(personaggio *p, uint8_t indiceOggetto);
-bool aggiungiEquipaggiamento(personaggio *p, oggetto *o);
-void calcolaStatistiche(personaggio *p, stats s);
-void freePersonaggioLink(personaggioLink *l);
 void freePersonaggio(personaggio *p);
+personaggio *creaPersonaggio(unsigned int nomeSize, unsigned int classeSize);
+void copiaPersonaggio(personaggio *dest, personaggio *src);
+void calcolaStatistiche(personaggio *p, stats s);
+bool aggiungiEquipaggiamento(personaggio *p, oggetto o, inventario i);
 void allocaPersonaggio(personaggio *p, unsigned int nomeSize, unsigned int classeSize);
 
 #endif // PERSONAGGIO_H
