fc25c071c61a8a20265c0b4ad28c3f7c110eb814
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-16 15:54:56 +0100

    UPDATE personaggioLink as ADT

--- L09/E02/listaPersonaggio.c
+++ L09/E02/listaPersonaggio.c
@@ -7,12 +7,87 @@ struct PersonaggioLink {
 
 const uint8_t PERSONAGGIO_MAX_STRING = 50;
 
+// Aggiunge un elemento next subito dopo un elemento l in una lista
+void addNext(personaggioLink l, personaggioLink next) {
+    if (l->Next == NULL) { // Se si tratta dell'ultimo elemento
+        l->Next    = next;
+        next->Next = NULL;
+        return;
+    }
+
+    personaggioLink temp = l->Next;
+    next->Next           = temp;
+    l->Next              = next;
+    return;
+}
+
+// Crea, alloca e restituisce un puntatore a personaggioLink
+personaggioLink creaLista(personaggio p) {
+    personaggioLink l = (personaggioLink)malloc(sizeof(personaggioLink));
+    l->Personaggio    = p;
+    return l;
+}
+
+// Salva un personaggio in un elemento di tipo personaggioLink
+personaggioLink creaPersonaggioLink(personaggio p) {
+    personaggioLink l = (personaggioLink)malloc(sizeof(struct PersonaggioLink));
+    l->Personaggio    = p;
+    return l;
+}
+
+// Estrare l'elemento successivo a quello dato da una lista, lo stampa e libera la memoria
+void eliminaAndPrint(personaggioLink previous) {
+    previous = estraiNext(previous);
+    printf("L'elemento eliminato è ---> ");
+    printPersonaggio(previous->Personaggio, stdout);
+    freePersonaggioLink(previous);
+}
+
+// Estrae l'elemento successivo in lista
+personaggioLink estraiNext(personaggioLink previous) {
+    personaggioLink elemento = previous->Next;
+    previous->Next           = elemento->Next;
+    return elemento;
+}
+
 // Dealloca la memoria di una lista ed il suo contenuto
 void freePersonaggioLink(personaggioLink l) {
     if (l->Personaggio != NULL) { // Se non sono sulla HEAD
         freePersonaggio(l->Personaggio);
     }
     free(l);
+}
+
+// Restituisce l'elemento successivo nella lista
+personaggioLink getNextItem(personaggioLink item) {
+    return item->Next == NULL ? NULL : item->Next;
+}
+
+// Legge i personaggi da file e li salva in una lista
+void parsePersonaggi(tabellaPersonaggio *TABLE, FILE *stream) {
+    personaggio temp = creaPersonaggio(PERSONAGGIO_MAX_STRING, PERSONAGGIO_MAX_STRING);
+    personaggioLink tempList;
+
+    // Creo stringa di appoggio
+    char string[(PERSONAGGIO_MAX_STRING * 3) + 1];
+    fgets(string, PERSONAGGIO_MAX_STRING * 3, stream); // Leggo la prima riga del file
+
+    while (leggiPersonaggio(string, temp)) { // Sinché leggo correttamente i personaggi
+        personaggio p = getResizedPersonaggio(temp);
+        copiaPersonaggio(p, temp);
+        tempList = creaPersonaggioLink(p);
+        addNext(TABLE->TAIL, tempList);
+        TABLE->NumeroPersonaggi++;
+        TABLE->TAIL = tempList;
+        fgets(string, PERSONAGGIO_MAX_STRING * 3, stream); // Leggo la riga successiva
+    }
+
+    freePersonaggio(temp);
+}
+
+// Stampa una lista di personaggi a video
+void printPersonaggioLink(personaggioLink l) {
+    printPersonaggioLinkFile(l, stdout);
 }
 
 // Stampa una lista di personaggi su file
@@ -26,52 +101,10 @@ void printPersonaggioLinkFile(personaggioLink l, FILE *stream) {
     printf("\n");
 }
 
-// Restituisce l'elemento successivo nella lista
-personaggioLink getNextItem(personaggioLink item) {
-    return item->Next == NULL ? NULL : item->Next;
-}
-
-// Crea, alloca e restituisce un puntatore a personaggioLink
-personaggioLink creaLista(personaggio *p) {
-    personaggioLink l = (personaggioLink)malloc(sizeof(personaggioLink));
-    l->Personaggio    = p;
-    return l;
-}
-
-// Legge i personaggi da file e li salva in una lista
-void parsePersonaggi(tabellaPersonaggio *TABLE, FILE *stream) {
-    personaggio *temp = creaPersonaggio(PERSONAGGIO_MAX_STRING, PERSONAGGIO_MAX_STRING);
-    personaggioLink tempList;
-
-    // Creo stringa di appoggio
-    char string[(PERSONAGGIO_MAX_STRING * 3) + 1];
-    fgets(string, PERSONAGGIO_MAX_STRING * 3, stream); // Leggo la prima riga del file
-
-    while (leggiPersonaggio(string, temp)) { // Sinché leggo correttamente i personaggi
-        personaggio *p = getResizedPersonaggio(temp);
-        copiaPersonaggio(p, temp);
-        tempList = creaPersonaggioLink(p);
-        addNext(TABLE->TAIL, tempList);
-        TABLE->NumeroPersonaggi++;
-        TABLE->TAIL = tempList;
-        fgets(string, PERSONAGGIO_MAX_STRING * 3, stream); // Leggo la riga successiva
-    }
-
-    freePersonaggio(temp);
-}
-
-// Aggiunge un elemento next subito dopo un elemento l in una lista
-void addNext(personaggioLink l, personaggioLink next) {
-    if (l->Next == NULL) { // Se si tratta dell'ultimo elemento
-        l->Next    = next;
-        next->Next = NULL;
-        return;
-    }
-
-    personaggioLink temp = l->Next;
-    next->Next           = temp;
-    l->Next              = next;
-    return;
+// Ricerca e restituisce un personaggio tramite ID
+personaggioLink ricercaID(personaggioLink HEAD, uint16_t *ID) {
+    personaggioLink precedente = ricercaIDprecedente(HEAD, ID);
+    return precedente == NULL ? NULL : precedente->Next;
 }
 
 // Ricerca ricorsiva che restituisce l'elemento precedete a quello trovato
@@ -90,36 +123,4 @@ personaggioLink ricercaIDprecedente(personaggioLink HEAD, uint16_t *ID) {
     }
 
     return ricercaIDprecedente(pointer, ID); // Ricerco sull'elemento successivo
-}
-
-personaggioLink ricercaID(personaggioLink HEAD, uint16_t *ID) {
-    personaggioLink precedente = ricercaIDprecedente(HEAD, ID);
-    return precedente == NULL ? NULL : precedente->Next;
-}
-
-// Estrae l'elemento successivo in lista
-personaggioLink estraiNext(personaggioLink previous) {
-    personaggioLink elemento = previous->Next;
-    previous->Next           = elemento->Next;
-    return elemento;
-}
-
-// Estrare l'elemento successivo a quello dato da una lista, lo stampa e libera la memoria
-void eliminaAndPrint(personaggioLink previous) {
-    previous = estraiNext(previous);
-    printf("L'elemento eliminato è ---> ");
-    printPersonaggio(previous->Personaggio, stdout);
-    freePersonaggioLink(previous);
-}
-
-// Salva un personaggio in un elemento di tipo personaggioLink
-personaggioLink creaPersonaggioLink(personaggio *p) {
-    personaggioLink l = (personaggioLink)malloc(sizeof(personaggioLink));
-    l->Personaggio    = p;
-    return l;
-}
-
-// Stampa una lista di personaggi a video
-void printPersonaggioLink(personaggioLink l) {
-    printPersonaggioLinkFile(l, stdout);
 }
--- L09/E02/listaPersonaggio.h
+++ L09/E02/listaPersonaggio.h
@@ -10,18 +10,18 @@ typedef struct TabellaPersonaggio {
     unsigned int NumeroPersonaggi;
 } tabellaPersonaggio;
 
-void printPersonaggioLink(personaggioLink l);
-void eliminaAndPrint(personaggioLink previous);
-personaggioLink estraiNext(personaggioLink previous);
-personaggioLink ricercaID(personaggioLink HEAD, uint16_t *ID);
 personaggioLink ricercaIDprecedente(personaggioLink HEAD, uint16_t *ID);
-void addNext(personaggioLink l, personaggioLink next);
-personaggioLink creaPersonaggioLink(personaggio *p);
-void parsePersonaggi(tabellaPersonaggio *TABLE, FILE *stream);
-personaggioLink creaLista(personaggio *p);
-personaggioLink getNextItem(personaggioLink item);
+personaggioLink ricercaID(personaggioLink HEAD, uint16_t *ID);
 void printPersonaggioLinkFile(personaggioLink l, FILE *stream);
+void printPersonaggioLink(personaggioLink l);
+void parsePersonaggi(tabellaPersonaggio *TABLE, FILE *stream);
+personaggioLink getNextItem(personaggioLink item);
 void freePersonaggioLink(personaggioLink l);
+personaggioLink estraiNext(personaggioLink previous);
+void eliminaAndPrint(personaggioLink previous);
+personaggioLink creaPersonaggioLink(personaggio p);
+personaggioLink creaLista(personaggio p);
+void addNext(personaggioLink l, personaggioLink next);
 extern const uint8_t PERSONAGGIO_MAX_STRING;
 
 #endif // LISTAPERSONAGGIO_H
