f555fec3726efe9547b1e02f2efed305a9bf9897
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-17 20:12:32 +0100

    rename variables

--- L09/E01/main.c
+++ L09/E01/main.c
@@ -7,7 +7,7 @@
 
 // Copia le attività della LA in un array di attività
 void copiaLA(arrayAttivita *a, arrayAttivita *risultato, int *posizioni, unsigned int ultimo) {
-    if (posizioni[ultimo] == -1) {
+    if (posizioni[ultimo] == (unsigned int)-1) {
         risultato->Array[risultato->NumeroElementi++] = a->Array[ultimo];
         return;
     }
@@ -15,37 +15,36 @@ void copiaLA(arrayAttivita *a, arrayAttivita *risultato, int *posizioni, unsigne
     risultato->Array[risultato->NumeroElementi++] = a->Array[ultimo];
 }
 
-unsigned int calcolaLA(arrayAttivita *att, int *P) {
-    unsigned int i, j, ris = 1, V[att->NumeroElementi], last = 1, TMP[att->NumeroElementi];
-    V[0] = att->Array[0].Durata;
-    P[0] = -1;
+unsigned int calcolaLA(arrayAttivita *att, unsigned int *posizioni) {
+    unsigned int i, j, ultimaDurata = 1, durate[att->NumeroElementi], last = 1, durateTemp[att->NumeroElementi];
+    durate[0]    = att->Array[0].Durata;
+    posizioni[0] = (unsigned int)-1;
 
     for (i = 1; i < (att->NumeroElementi); i++) {
-        TMP[i] = att->Array[i].Durata;
-        V[i]   = att->Array[i].Durata;
-        P[i]   = -1;
+        durateTemp[i] = att->Array[i].Durata;
+        durate[i]     = att->Array[i].Durata;
+        posizioni[i]  = -1;
         for (j = 0; j < i; j++)
-            if (!attivitaSovvrapposta(&att->Array[i], &att->Array[j]) && (V[i] < TMP[i] + V[j])) {
-                V[i] = TMP[i] + V[j];
-                P[i] = j;
+            if (!attivitaSovvrapposta(&att->Array[i], &att->Array[j]) && (durate[i] < durateTemp[i] + durate[j])) {
+                durate[i]    = durateTemp[i] + durate[j];
+                posizioni[i] = j;
             }
-        if (ris <= V[i]) {
-            ris  = V[i];
-            last = i;
+        if (ultimaDurata <= durate[i]) {
+            ultimaDurata = durate[i];
+            last         = i;
         }
     }
-    printf("Lunghezza: %d\nSequenza attività: ", ris);
     return last;
 }
 
 void selezioneDinamica(arrayAttivita *a) {
     ordina(a);
-    int P[a->NumeroElementi];
-    unsigned int ultimo = calcolaLA(a, P);
+    unsigned int posizioni[a->NumeroElementi];
+    unsigned int ultimo = calcolaLA(a, posizioni);
     arrayAttivita risultato;
     risultato.Array          = (attivita *)calloc(a->NumeroElementi, sizeof(attivita));
     risultato.NumeroElementi = 0;
-    copiaLA(a, &risultato, P, ultimo);
+    copiaLA(a, &risultato, posizioni, ultimo);
     durataArrayAttivita(&risultato);
     printArrayAttivita(&risultato);
     printf("\n");
