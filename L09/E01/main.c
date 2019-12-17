5bab77002aa1783d985e702e4a50962cbf1a1097
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-17 19:47:01 +0100

    REMOVE calcolo combinatorio

--- L09/E01/main.c
+++ L09/E01/main.c
@@ -5,105 +5,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// * CALCOLO COMBINATORIO
-
-// Powerset
-unsigned int powerset(uint8_t posizione, arrayAttivita *a, attivita *soluzione,
-                      uint8_t start, arrayAttivita *max) {
-    unsigned int count = 0;
-
-    if (start >= a->NumeroElementi) { // Se ho raggiunto la lunghezza massima
-        if (posizione > 0) {          // Se ho almeno un elemento
-            arrayAttivita temp;
-            temp.Array          = soluzione;
-            temp.NumeroElementi = posizione;
-            durataArrayAttivita(&temp);
-            if (temp.Durata > max->Durata) {
-                copiaArrayAttivita(max, &temp);
-            }
-            return 1;
-        }
-        return 0;
-    }
-
-    for (uint8_t i = start; i < a->NumeroElementi; i++) { // Per ogni posizione rimanente
-        if (posizione > 0) {                              // Se ho almeno un'attività giaà presente nelle soluzioni
-
-            while (attivitaSovvrapposta(&soluzione[posizione - 1], &a->Array[i]) && i < a->NumeroElementi) { // Sinché l'attività successiva è sovrapposta
-                i++;
-            }
-        }
-
-        if (i < a->NumeroElementi) { // Se la condizione è ancora valida
-            soluzione[posizione] = a->Array[i];
-            count += powerset(posizione + 1, a, soluzione, i + 1, max);
-        }
-    }
-
-    count += powerset(posizione, a, soluzione, a->NumeroElementi, max);
-    return count;
-}
-// Stampa le attività selezionate
-void attSel(int N, attivita *v) {
-    attivita b[N];
-    arrayAttivita a, max;
-    max.Array          = (attivita *)calloc(N, sizeof(attivita));
-    max.Durata         = 0;
-    max.NumeroElementi = 0;
-    a.Array            = v;
-    a.NumeroElementi   = N;
-    durataArrayAttivita(&a);
-
-    unsigned int risultati = powerset(0, &a, b, 0, &max);
-    printf("Sono state generate %d combinazioni di attività, quella di durata massima è\n", risultati);
-    printArrayAttivita(&max);
-}
-// * -------------------------------------------------------------
-
-void scriviLIS(arrayAttivita *valori, unsigned int *posizioni, unsigned int index, arrayAttivita *LIS) {
-    if (index == 0) {
+void LIA_display(arrayAttivita *att, int *P, int i) {
+    if (P[i] == -1) {
+        printf("(%d,%d) ", att->Array[i].Inizio, att->Array[i].Fine);
         return;
     }
-
-    LIS->Array[LIS->NumeroElementi++] = valori->Array[index - 1];
-    index                             = posizioni[index - 1];
-    scriviLIS(valori, posizioni, index, LIS);
+    LIA_display(att, P, P[i]);
+    printf("(%d,%d) ", att->Array[i].Inizio, att->Array[i].Fine);
 }
 
-unsigned int LISS(arrayAttivita *valori, unsigned int *posizioni, unsigned int *LIS) {
-    unsigned int precedente       = 0;
-    unsigned int precedenteIndex  = 0;
-    unsigned int lunghezzaMassima = 1;
-    for (unsigned int i = 0; i < valori->NumeroElementi; i++) { // Per ogni elemento
-        if (valori->Array[i].Inizio < precedente) {             // Se il valore interrompe la sequenza
-            // Inserisco i valori di interruzione
-            posizioni[i] = 0;
-            LIS[i]       = valori->Array[i].Durata;
-        } else {
-            // Proseguo l'incremento della LIS
-            posizioni[i]    = precedenteIndex;
-            precedenteIndex = i + 1;
-            LIS[i] += valori->Array[i].Durata;
-            lunghezzaMassima += LIS[i] >
-        }
-        precedente = valori->Array[i].Fine;
-    }
+void LIA_calc(arrayAttivita *att) {
+    int i, j, ris = 1, V[att->NumeroElementi], P[att->NumeroElementi], last = 1, TMP[att->NumeroElementi];
+    V[0] = att->Array[0].Durata;
+    P[0] = -1;
+    TMP[0];
 
-    return precedenteIndex; // Restituisco l'ultimo valore della LIS
+    for (i = 1; i < (att->NumeroElementi); i++) {
+        TMP[i] = att->Array[i].Durata;
+        V[i]   = att->Array[i].Durata;
+        P[i]   = -1;
+        for (j = 0; j < i; j++)
+            if (attivitaSovvrapposta(&att->Array[i], &att->Array[j]) && (V[i] < TMP[i] + V[j])) {
+                V[i] = TMP[i] + V[j];
+                P[i] = j;
+            }
+        if (ris <= V[i]) {
+            ris  = V[i];
+            last = i;
+        }
+    }
+    printf("Lunghezza: %d\nSequenza attività: ", ris);
+    LIA_display(att, P, last);
 }
 
 void selezioneDinamica(arrayAttivita *a) {
-    unsigned int posizioni[a->NumeroElementi];
-    unsigned int LIS[a->NumeroElementi];
-    arrayAttivita risultati;
-    risultati.NumeroElementi = 0;
-    attivita temp[a->NumeroElementi];
-    risultati.Array = temp;
-
-    unsigned int index = LISS(a, posizioni, LIS);
-    scriviLIS(a, posizioni, index, &risultati);
-    durataArrayAttivita(&risultati);
-    printArrayAttivita(&risultati);
+    ordina(a);
+    LIA_calc(a);
 }
 
 int main() {
@@ -120,5 +57,7 @@ int main() {
     selezioneDinamica(&a);
     puts("OLD");
     attSel(a.NumeroElementi, a.Array);
+    puts("\nDinamica:\n");
+    selezioneDinamica(&a);
     return 0;
 }
