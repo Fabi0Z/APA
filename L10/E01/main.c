5165ce4f99611c403d0f7907d3fca9057065acf3
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-23 18:11:19 +0100

    UPDATE parseFromFile

--- L10/E01/main.c
+++ L10/E01/main.c
@@ -7,21 +7,6 @@ const uint8_t MAX_FILENAME = 51;
 void parseFromFile(char *filename) { // Esegue i vari test presenti in un file
     FILE *stream = smartFopen(filename, "r");
     unsigned int nTest, massima;
-    collana max;
-    fscanf(stream, "%u\n", &nTest);
-    for (unsigned int i = 0; i < nTest; i++) { // Per ogni test
-        char riga[MAX_FILENAME];
-        fgets(riga, MAX_FILENAME - 1, stream);
-        collana c = parseCollana(riga);
-        printf("TEST #%d\n", i + 1);
-        collaneVarieLunghezze(&c, &max);
-        printCollana(&max);
-    }
-}
-
-void parseFromFile2(char *filename) { // Esegue i vari test presenti in un file
-    FILE *stream = smartFopen(filename, "r");
-    unsigned int nTest, massima;
     fscanf(stream, "%u\n", &nTest);
     for (unsigned int i = 0; i < nTest; i++) { // Per ogni test
         char riga[MAX_FILENAME];
@@ -31,14 +16,16 @@ void parseFromFile2(char *filename) { // Esegue i vari test presenti in un file
         unsigned int lunghezza = maxCollana(c.Pietre);
         printf("Lunghezza massima: %d\n", lunghezza);
     }
+    fclose(stream);
 }
 
 int main() {
-    char filename[MAX_FILENAME];
-    puts("Inserisci il nome del file:");
-    printf("==> ");
-    fgets(filename, (MAX_FILENAME - 1), stdin);
-    sscanf(filename, "%s", filename);
-    parseFromFile2(filename);
+    // char filename[MAX_FILENAME];
+    // puts("Inserisci il nome del file:");
+    // printf("==> ");
+    char filename[51] = "e1_easy_test_set.txt";
+    // fgets(filename, (MAX_FILENAME - 1), stdin);
+    // sscanf(filename, "%s", filename);
+    parseFromFile(filename);
     return 0;
 }
