8e936f4b586a006d86fa93b8b76c68a8e0286c9d
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-27 10:04:07 +0100

    ADD main example

--- L10/E02/main.c
+++ L10/E02/main.c
@@ -5,7 +5,10 @@ const uint8_t MAX_STRING = 150;
 
 int main() {
     char filename[] = "elementi.txt";
-    array elementi  = parseFromFile(filename, MAX_STRING, (void *)&parseElemento);
-    elemento *e     = (elemento *)elementi->Elementi;
+    // arrayElemento elementi = parseElementoFromFile(filename);
+    // printElemento(elementi->Array[0]);
+    array elementi = parseFromFile(filename, MAX_STRING, (void *)&parseElemento);
+    printArray(elementi, &printElemento);
+    freeArray(elementi, &freeElemento);
     return 0;
 }
