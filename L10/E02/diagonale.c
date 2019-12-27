63fa90bff69b407886b1784bb8955976e3b5fbf6
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-27 14:17:54 +0100

    ADD const for numero diagonali

--- L10/E02/diagonale.c
+++ L10/E02/diagonale.c
@@ -1,16 +1,18 @@
 #include "diagonale.h"
 
-// Crea un gruppo di 3 diagonali
+static const uint8_t NUMERO_DIAGONALI = 3;
+
+// Crea un gruppo di NUMERO_DIAGONALI diagonali
 gruppoDiagonali creaGruppoDiagonali() {
-    gruppoDiagonali g = calloc(3, sizeof(array));
-    for (uint8_t i = 0; i < 3; i++) {
+    gruppoDiagonali g = calloc(NUMERO_DIAGONALI, sizeof(array));
+    for (uint8_t i = 0; i < NUMERO_DIAGONALI; i++) {
         g[i] = creaArray();
     }
 }
 
 // Elimina un gruppo di diagonali
 void freeGruppoDiagonali(gruppoDiagonali g) {
-    for (uint8_t i = 0; i < 3; i++) {
+    for (uint8_t i = 0; i < NUMERO_DIAGONALI; i++) {
         g[i]->free(g[i], (void *)&freeElemento);
     }
 }
