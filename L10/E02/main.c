6f47daaef496a49567a7c4da92537083af1417e4
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-12-30 08:59:23 +0100

    include programma

--- L10/E02/main.c
+++ L10/E02/main.c
@@ -1,4 +1,3 @@
-#include "calcoloCombinatorio.h"
 #include "programma.h"
 
 const uint8_t MAX_STRING = 150;
@@ -7,10 +6,11 @@ int main() {
     char filename[] = "elementi.txt";
     array elementi  = creaArray((void *)&freeElemento, (void *)&parseElemento, (void *)&printElemento);
     parseArrayFromFile(elementi, filename, MAX_STRING);
-    array soluzione = creaArray((void *)&freeElemento, (void *)&parseElemento, (void *)&printElemento);
-    allocaArray(soluzione, elementi->ObjectsNumber);
-    diposizioniRipetuteDefault(elementi, soluzione);
-    freeArray(soluzione, false);
+    printArray(elementi);
+
+    programma soluzione = creaProgramma();
+
+    freeProgramma(soluzione);
     freeArray(elementi, true);
     return 0;
 }
