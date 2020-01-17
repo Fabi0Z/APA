e82455df41d66a5917aedc65e6919f68832d82a5
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-01-17 23:56:17 +0100

    ADD update symblename

--- libs/SymbleTable/Symble.c
+++ libs/SymbleTable/Symble.c
@@ -1,14 +1,36 @@
 #include "Symble.h"
 
-// Crea un symble
-symble newSymble(unsigned int NameLength) {
-    symble s = malloc(sizeof(struct Symble));
-    s->Name  = calloc(NameLength + 1, sizeof(char));
-    return s;
+// Ridimensiona il nome correttamente
+static void fixNameSize(symble s, char *NewName) {
+    unsigned int Length = strlen(NewName);
+    if (Length > s->__NameLength) {
+        char temp[s->__NameLength];
+        strcpy(s->Name, temp);
+        free(s->Name);
+        s->__NameLength = Length;
+        s->Name         = calloc(s->__NameLength + 1, sizeof(char));
+    }
 }
 
 // Elimina un symble
 void freeSymble(symble s) {
+    if (s == NULL) {
+        return;
+    }
     free(s->Name);
     free(s);
+}
+
+// Crea un symble
+symble newSymble(unsigned int NameLength) {
+    symble s        = malloc(sizeof(struct Symble));
+    s->__NameLength = NameLength;
+    s->Name         = calloc(s->__NameLength + 1, sizeof(char));
+    return s;
+}
+
+// Aggiorna il nome di un Symble
+void updateSymbleName(symble s, char *NewName) {
+    fixNameSize(s, NewName);
+    strcpy(s->Name, NewName);
 }
--- libs/SymbleTable/Symble.h
+++ libs/SymbleTable/Symble.h
@@ -2,13 +2,16 @@
 #define SYMBLE_H
 
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Symble {
     unsigned int Index;
     char *Name;
+    unsigned int __NameLength;
 } * symble;
 
-void freeSymble(symble s);
+void updateSymbleName(symble s, char *NewName);
 symble newSymble(unsigned int NameLength);
+void freeSymble(symble s);
 
 #endif // ! SYMBLE_H
